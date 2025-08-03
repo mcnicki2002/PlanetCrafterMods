cd9d6c83ad8a2841620c7ef1e52141520a0e44d7
mcnicki2002 <119167106+mcnicki2002@users.noreply.github.com>
2025-08-03 16:04:00 +0200

    Added enable config + lock in main screen

--- UiConfineMouse/Plugin.cs
+++ UiConfineMouse/Plugin.cs
@@ -2,6 +2,7 @@
 // Licensed under Apache License, Version 2.0
 
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using SpaceCraft;
 using UnityEngine;
@@ -10,11 +11,14 @@ using UnityEngine;
 namespace Nicki0.UiConfineMouse {
 	[BepInPlugin("Nicki0.theplanetcraftermods.UiConfineMouse", "(UI) Confine Mouse", PluginInfo.PLUGIN_VERSION)]
 	public class Plugin : BaseUnityPlugin {
-
+		public static ConfigEntry<bool> enableMod;
 		private void Awake() {
 			// Plugin startup logic
-			Harmony.CreateAndPatchAll(typeof(Plugin));
-
+			enableMod = Config.Bind<bool>("Config", "enable", true, "Enable mod (requires restart to take effekt)");
+			if (enableMod.Value) {
+				Harmony.CreateAndPatchAll(typeof(Plugin));
+				Cursor.lockState = CursorLockMode.Confined;
+			}
 			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 		}
 		[HarmonyPrefix] // Prefix + return false so Cursor.lockState isn't called *again* in a postfix, of which I don't know how it could impact performance.
