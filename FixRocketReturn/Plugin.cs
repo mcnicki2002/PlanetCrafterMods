98538e053aa602aee1429e6afa099670ab7c44c4
mcnicki2002 <119167106+mcnicki2002@users.noreply.github.com>
2025-11-24 01:45:11 +0100

    Release Fix for float comparison error when landing for off-grid rockets

---
+++ FixRocketReturn/Plugin.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2025-2025 Nicolas Schäfer & Contributors
+// Licensed under Apache License, Version 2.0
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SpaceCraft;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Nicki0.FixRocketReturn {
+	[BepInPlugin("Nicki0.theplanetcraftermods.FixRocketReturn", "(Fix) Rocket Return", PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin {
+		static ManualLogSource log;
+
+		private void Awake() {
+			// Plugin startup logic
+			log = Logger;
+
+			Harmony.CreateAndPatchAll(typeof(Plugin));
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+		}
+
+		private static Vector3 epsilonDown = new Vector3(0, -0.001f, 0);
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(MachineRocketBackAndForth), "SetRocketUpThereAndDisableIt")]
+		public static void MachineRocketBackAndForth_SetRocketUpThereAndDisableIt(GameObject ___rocket) {
+			if (___rocket != null && ___rocket.transform != null) ___rocket.transform.position += epsilonDown;
+		}
+
+	}
+}
