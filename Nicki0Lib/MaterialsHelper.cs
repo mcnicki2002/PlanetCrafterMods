dd1639803f955553a6a142586a445a3508a91bef
mcnicki2002 <119167106+mcnicki2002@users.noreply.github.com>
2026-02-12 19:49:17 +0100

    Added overwrite clone materials + more materials

--- Nicki0Lib/MaterialsHelper.cs
+++ Nicki0Lib/MaterialsHelper.cs
@@ -6,7 +6,12 @@ using HarmonyLib;
 using SpaceCraft;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Nicki0 {
 
@@ -15,6 +20,8 @@ namespace Nicki0 {
 
 		class Nicki0_MaterialsHelper : MonoBehaviour {
 			public Dictionary<string, Material> materialDictionary;
+			public Dictionary<string, Material> completeMaterialDictionary;
+			public Dictionary<string, Material> completeMaterialDictionary2;
 		}
 		static GameObject materialsHelperObject;
 		public static void InitMaterialsHelper(ManualLogSource pLog) {
@@ -32,7 +39,8 @@ namespace Nicki0 {
 			}
 		}
 
-		public static bool ApplyGameMaterials(GameObject toFix) {
+
+		public static bool ApplyGameMaterials(GameObject toFix, bool normalizeName = false, bool fromCompleteCollection = false) {
 			if (materialsHelperObject == null) {
 				Console.WriteLine("[Fatal] MaterialsHelper not initialized");
 				return false;
@@ -44,17 +52,23 @@ namespace Nicki0 {
 
 			Nicki0_MaterialsHelper materialsHelper = materialsHelperObject.GetComponent<Nicki0_MaterialsHelper>();
 
-			foreach (MeshRenderer mr in toFix.GetComponentsInChildren<MeshRenderer>()) {
+			foreach (Renderer mr in toFix.GetComponentsInChildren<Renderer>()) {
 				Material[] materials = mr.GetSharedMaterialArray();
 
 				for (int i = 0; i < materials.Length; i++) {
-					if (materialsHelper.materialDictionary.TryGetValue(materials[i].name, out Material gameMaterial)) {
+					if (fromCompleteCollection) {
+						if (materialsHelper.completeMaterialDictionary.TryGetValue(normalizeName ? materials[i].name.CanonicalizeString() : materials[i].name, out Material gameMaterial)) {
+							materials[i] = gameMaterial;
+						}
+					} else if (materialsHelper.materialDictionary.TryGetValue(normalizeName ? materials[i].name.CanonicalizeString() : materials[i].name, out Material gameMaterial)) {
 						materials[i] = gameMaterial;
 					}
 				}
 
-				mr.SetMaterialArray(materials);
+				//mr.SetMaterialArray(materials);
+				mr.SetSharedMaterials(materials.ToList()); // Difference???
 			}
+
 			return true;
 		}
 
@@ -62,24 +76,136 @@ namespace Nicki0 {
 		[HarmonyPriority(Priority.First)]
 		[HarmonyPatch(typeof(StaticDataHandler), "LoadStaticData")]
 		private static void StaticDataHandler_LoadStaticData(List<GroupData> ___groupsData) {
+			Stopwatch timer = Stopwatch.StartNew();
+
 			Nicki0_MaterialsHelper materialsHelper = materialsHelperObject.GetComponent<Nicki0_MaterialsHelper>();
 			if (materialsHelper.materialDictionary != null) {
 				return;
 			}
 			materialsHelper.materialDictionary = new Dictionary<string, Material>();
+			materialsHelper.completeMaterialDictionary = new Dictionary<string, Material>();
+			materialsHelper.completeMaterialDictionary2 = new Dictionary<string, Material>();
 
+			// --- Add materials from GroupDataConstructable ---
+			// ~18ms
 			foreach (GroupData gd in ___groupsData) {
 				if (gd == null || gd.associatedGameObject == null) continue;
 
-				foreach (MeshRenderer mr in gd.associatedGameObject.GetComponentsInChildren<MeshRenderer>()) {
-					if (mr == null) continue;
+				//foreach (MeshRenderer renderer in gd.associatedGameObject.GetComponentsInChildren<MeshRenderer>()) {
+				foreach (Renderer renderer in gd.associatedGameObject.GetComponentsInChildren<Renderer>()) {
+					if (renderer == null) continue;
 
-					foreach (Material material in mr.GetSharedMaterialArray()) {
+					foreach (Material material in renderer.GetSharedMaterialArray()) {
 						if (material == null) continue;
 						materialsHelper.materialDictionary.TryAdd(material.name, material);
 					}
 				}
 			}
+
+			// ~0.4ms
+			foreach (MethodInfo mi in AccessTools.GetDeclaredMethods(typeof(VisualsResourcesHandler))) {
+				if (mi.ReturnType == typeof(Material) && mi.GetParameters().Length == 0) {
+					Material returnedMaterial = mi.Invoke(Managers.GetManager<VisualsResourcesHandler>(), []) as Material;
+					if (returnedMaterial == null || string.IsNullOrEmpty(returnedMaterial.name)) continue;
+					materialsHelper.materialDictionary.TryAdd(returnedMaterial.name, returnedMaterial);
+				}
+			}
+			
+
+			/*
+			// --- Add materials from procedural wrecks --- Does not add any additional Materials ---
+			//ProceduralInstancesHandler.GeneratorData[] _generators
+			Type generatorDataType = AccessTools.FirstInner(typeof(ProceduralInstancesHandler), x => x.Name.Contains("GeneratorData"));
+			Array generatorArray = (Array)(AccessTools.GetDeclaredFields(typeof(ProceduralInstancesHandler)).Where(x => x.Name == "_generators").First().GetValue(ProceduralInstancesHandler.Instance));
+			FieldInfo GOProperty = AccessTools.Field(generatorDataType, "generator");
+			for (int i = 0; i < generatorArray.Length; i++) {
+				GameObject go = ((GameObject)GOProperty.GetValue(generatorArray.GetValue(i)));
+				if (go == null) continue;
+
+				foreach (MeshRenderer renderer in go.GetComponentsInChildren<MeshRenderer>()) {
+					//foreach (Renderer renderer in go.GetComponentsInChildren<Renderer>()) {
+					if (renderer == null) continue;
+
+					foreach (Material material in renderer.GetSharedMaterialArray()) {
+						if (material == null) continue;
+						materialsHelper.materialDictionary.TryAdd(material.name, material);
+					}
+				}
+			}*/
+
+
+
+
+			/*
+			// --- Add materials from all Scenes ---
+			for (int i = 0; i < SceneManager.sceneCount; i++) {
+				Scene scene = SceneManager.GetSceneAt(i);
+				if (scene == null) { continue; }
+				foreach (GameObject go in scene.GetRootGameObjects()) {
+					if (go == null) continue;
+					foreach (Renderer r in go.GetComponentsInChildren<Renderer>()) {
+						if (r == null) continue;
+						foreach (Material m in r.sharedMaterials) {
+							if (m == null) continue;
+							materialsHelper.materialDictionary.TryAdd(m.name, m);
+						}
+					}
+				}
+			}
+
+			
+
+			// --- Add materials of all Renderers ---
+			foreach (Renderer r in Resources.FindObjectsOfTypeAll(typeof(Renderer))) {
+				if (r == null) continue;
+				foreach (Material m in r.sharedMaterials) {
+					if (m == null) continue;
+					materialsHelper.materialDictionary.TryAdd(m.name.Replace("(Instance)", "").Replace("(Clone)", "").Trim(), m);
+				}
+			}
+
+			foreach (Renderer renderer in UnityEngine.Object.FindObjectsByType<Renderer>(FindObjectsInactive.Include, FindObjectsSortMode.None)) {
+				if (renderer == null) continue;
+
+				foreach (Material m in renderer.sharedMaterials) {
+					if (m == null) continue;
+					materialsHelper.materialDictionary.TryAdd(m.name.Replace("(Instance)", "").Replace("(Clone)", "").Trim(), m);
+				}
+			}
+
+			// --- Add materials from MaterialList objects ---
+			foreach (MaterialList ml in UnityEngine.Object.FindObjectsByType(typeof(MaterialList), FindObjectsSortMode.None)) {
+				if (ml == null) continue;
+
+				foreach (Material m in ml.materials) {
+					materialsHelper.materialDictionary.TryAdd(m.name, m);
+				}
+			}
+			*/
+
+			// --- Add materials of all Materials ---
+			// ~0.6 ms
+			foreach (Material m in Resources.FindObjectsOfTypeAll(typeof(Material))) {
+				if (m == null) continue;
+				materialsHelper.completeMaterialDictionary2.TryAdd(m.name, m);
+			}
+			
+			// ~70ms
+			/*foreach (UnityEngine.Object obj in Resources.FindObjectsOfTypeAll<UnityEngine.Object>()) {
+				if (obj is Material m) {
+					materialsHelper.completeMaterialDictionary.TryAdd(m.name, m);
+				}
+			}*/
+			
+
+
+			log.LogInfo("Material collection took: " + timer.Elapsed.TotalMilliseconds + " ms");
+		}
+
+	}
+	public static class StringExtension {
+		public static string CanonicalizeString(this string str) {
+			return str.Replace("(Instance)", "").Replace("(Clone)", "").Trim();
 		}
 	}
 }
